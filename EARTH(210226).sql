6cca21359051801f87c5438708fe7e5cb55370f4
gamhj <78398192+gamhj@users.noreply.github.com>
2021-03-02 18:39:00 +0900

    업체테이블 컬럼변경

--- EARTH(210226).sql
+++ EARTH(210226).sql
@@ -20,3 +20,18 @@ BEGIN
     WHERE AA_BNO = (SELECT AA_BNO FROM AUC_TB WHERE A_STATE = '준비중');
 END;
 /
+
+--210302
+
+--업체테이블 c_email = m_email로 변경
+
+alter table auc_cmpny_tb rename column c_email to m_email;
+
+alter table auc_cmpny_tb modify m_email varchar2(100);
+
+
+--업체테이블 m_email 외래키 생성
+
+ALTER TABLE auc_cmpny_tb
+    ADD CONSTRAINT fk_mber_tb_to_auc_cmpny_tb_1 FOREIGN KEY ( m_email )
+        REFERENCES mber_tb ( m_email );
